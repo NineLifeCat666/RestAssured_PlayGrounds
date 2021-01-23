5e907a25000c0ef0e3f90c737e02535a0f3b7445
nlc <NineLifeCat@yandex.ru>
2021-01-23 13:52:28 +0300

    refactor test successfulHasItemInlListUsersTest

--- src/test/java/ReqresInTests.java
+++ src/test/java/ReqresInTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
 
 public class ReqresInTests {
 
@@ -20,13 +21,13 @@ public class ReqresInTests {
     накидать сценарии,что можно тут проверить?
     при тестировании api RestAssured
     1. Что такой - то эдемент в data равен проверяемому
-    2. Что такой - то элемент содержится в data
+    2. Что такой - то элемент содержится в data +
     3. Что такой - то элемент не равен нулю
     4. извлечения значений после валидации (rest assured documentation)
      */
 
     @Test
-    void successfulEqualToInlListUsersTest() {
+    void successfulHasItemInlListUsersTest() {
         String contentType = "application/json; charset=utf-8";
         String cookie = "__cfduid=d13130dc652d8c9706f805b3da4628d481611168472; _ga=GA1.2.476192070.1611396245; _gid=GA1.2.1007837304.1611396245; _gat=1; _gat_gtag_UA_174008107_1=1; __stripe_mid=64a7ba76-7ef0-403a-8d6d-9f2e950f1fcd39b9c4; __stripe_sid=7486229b-e59f-4720-b237-381c502ec166d85208";
 
@@ -38,6 +39,6 @@ public class ReqresInTests {
                 .then()
                 .statusCode(200)
                 .log().body()
-                .body("data.last_name", equalTo("Lawson"));
+                .body("data.last_name", hasItem("Lawson"));
     }
 }
